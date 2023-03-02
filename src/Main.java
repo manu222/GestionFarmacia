1558086b46cc15b53008a33033028211f80967c8
manuel araujo baño <manuaraba@gmail.com>
2023-03-02 12:42:48 +0100

    Base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Farmaceutico farm1 = new Farmaceutico("Pepe",24,"Empleado",1900,"pepe@gmail.com",200,5.1);
+        System.out.println(farm1);
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,21 @@
 public class Persona {
+    String nombre;
+    int edad;
+    String type;
+
+
+    public Persona(String nombre, int edad,String type ){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.type=type;
+    }
+
+
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", type=" + type +
+                '}';
+    }
 }
